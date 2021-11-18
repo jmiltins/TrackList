20dbd380278cba19b87ba7190f4edd74ca5da573
jmiltins <miltins.janis@gmail.com>
2021-11-18 04:09:26 +0200

    movie detail view Crash fixed

--- TrackList/Controllers/DetailMovieViewController.swift
+++ TrackList/Controllers/DetailMovieViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailMovieViewController: UITableViewController {
+class DetailMovieViewController: UIViewController {
 
     @IBOutlet weak var movieImageViev: UIImageView!
     
